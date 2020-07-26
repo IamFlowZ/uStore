34751a6aeeec9b98d4306f487d05e125c32258da
iamflowz <dakota.lewallen17@gmail.com>
2020-07-26 17:30:17 -0400

    setting up display grid

--- frontend/src/components/Grid.js
+++ frontend/src/components/Grid.js
@@ -1,7 +1,32 @@
 import React, { useState } from "react";
+import { Grid, Paper } from "@material-ui/core";
+import UploadCard from "./UploadCard";
 
-const Grid = () => {
-  return <div>Grid</div>;
+const DisplayGrid = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "flex-start",
+        width: "90%",
+        height: "80%",
+      }}
+    >
+      <Paper style={{ width: "100%" }}>
+        <Grid container spacing={3}>
+          <UploadCard />
+          <UploadCard />
+          <UploadCard />
+          <UploadCard />
+          <UploadCard />
+          <UploadCard />
+          <UploadCard />
+          <UploadCard />
+          <UploadCard />
+        </Grid>
+      </Paper>
+    </div>
+  );
 };
 
-export default Grid;
+export default DisplayGrid;
---
+++ frontend/src/components/UploadCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Grid, Paper } from "@material-ui/core";
+
+const UploadCard = () => {
+  return (
+    <Grid item xs={3}>
+      <Paper>Hello</Paper>
+    </Grid>
+  );
+};
+
+export default UploadCard;
